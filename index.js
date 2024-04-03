3b6116999d7ef4f8c44366a01d38cef11f8af690
Pruthuvi Fernando <pruthuvi.fernando@gmail.com>
2024-04-03 16:37:14 +0530

    form validation halfway done

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-	let form = document.getElementById("form");
+	const form = document.getElementById("form");
+	const postcodeField = document.getElementById("postcode");
+	const propTypeField = document.getElementById("property-type");
+	const bedRoomsField = document.getElementById("bedrooms-field");
+	const minPrice = document.getElementById("min-price");
+	const maxPrice = document.getElementById("max-price");
+	const dateField = document.getElementById("date-added");
+	const submitbtn = document.querySelector("button[type=submit]");
+	const postCodeRegex = /^[A-Z]{1,2}[0-9][A-Z0-9]?/;
+
 	let container = document.getElementById("recent-properties");
-	let cardTemplate = document.querySelector("[data-card-template]");
+	let cardTemplate = document.getElementById("card-template");
+
 	let propJson = document.getElementById('property-data');
 	var propString = propJson.textContent.trim();
-
 	let properties = JSON.parse(propString);
 
 	let favouritePropsTitle = document.getElementById("recent-properties-title");
@@ -21,21 +30,126 @@ document.addEventListener("DOMContentLoaded", function () {
 		favouritePropsTitle.innerText = "Your favourite properties";
 	}
 
-	const propertyTileCreator = (prop) => {
+	properties.map(prop => {
+		if (prop.favourite) {
+			propertyTileCreator(prop);
+		}
+	});
 
-		const card = cardTemplate.content.cloneNode(true).children[0];
+	// Start -- form validation
+	postcodeField.addEventListener("change", (e) => {
+		let value = e.target.value.toUpperCase();
+		// console.log(value);
+		if (!value.match(postCodeRegex)) {
+			e.target.classList.remove("is-valid");
+			e.target.classList.add("is-invalid");
+			e.target.nextElementSibling.textContent = "Enter a valid postal code.";
+		} else {
+			e.target.classList.remove("is-invalid");
+			e.target.classList.add("is-valid");
+			e.target.nextElementSibling.textContent = "";
+		}
+	});
+
+	propTypeField.addEventListener("change", (e) => {
+		let value = e.target.value;
+		// console.log(value);
+		if (!value) {
+			e.target.classList.remove("is-valid");
+			e.target.classList.add("is-invalid");
+			e.target.nextElementSibling.textContent = "Please select a property type.";
+		} else {
+			e.target.classList.remove("is-invalid");
+			e.target.classList.add("is-valid");
+			e.target.nextElementSibling.textContent = "";
+		}
+	});
+
+	bedRoomsField.addEventListener("change", (e) => {
+		let value = parseInt(e.target.value);
+
+		if (typeof (value) === 'number' && value > 0) {
+			e.target.classList.remove("is-invalid");
+			e.target.classList.add("is-valid");
+			e.target.nextElementSibling.textContent = "";
+		} else {
+			e.target.classList.remove("is-valid");
+			e.target.classList.add("is-invalid");
+			e.target.nextElementSibling.textContent = "Please enter a number greater than 0";
+		}
+	});
+
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+
+		let formData = new FormData(form, submitbtn);
+
+		let formDataObj = {
+			postcode: "",
+			propType: "",
+			bedrooms: "",
+			minPrice: "",
+			maxPrice: "",
+			dateAdded: "1970-01-01"
+		}
+
+		for (const [key, value] of formData) {
+			if (key === "postcode") {
+				formDataObj.postcode = value;
+			}
+
+			if (key === "property-type") {
+				formDataObj.propType = value.toLowerCase();
+			}
+
+			if (key === "bedrooms") {
+				formDataObj.bedrooms = parseInt(value);
+			}
+
+			if (key === "min-price") {
+				formDataObj.minPrice = parseInt(value) || 0;
+			}
+
+			if (key === "max-price") {
+				formDataObj.maxPrice = parseInt(value) || Number.MAX_VALUE;
+			}
+
+			if (key === "date-added") {
+				formDataObj.dateAdded = value || formDataObj.dateAdded;
+			}
+		}
+
+		console.log(formDataObj);
+
+		let results = properties.filter(
+			prop => prop.postcode === formDataObj.postcode
+				&& prop.type.toLowerCase() === formDataObj.propType.toLowerCase()
+				&& prop.bedrooms === formDataObj.bedrooms
+		);
+
+		console.log(results);
+
+		localStorage.setItem("searchResults", JSON.stringify(results));
+
+		window.location.href = "./search-results.html";
+	});
+
+	// End -- form validation
+
+	function propertyTileCreator(prop) {
+
+		const card = cardTemplate.content.cloneNode(true);
 
 		// create the link
-		let linkElm = card.querySelector("[data-card-link]");
+		const linkElm = card.querySelector("[data-card-link]");
 		linkElm.setAttribute("href", prop.url || "#");
-		console.log(linkElm);
 
 		//create title
 		let propTitle = card.querySelector(".property-description");
 		propTitle.innerText = prop.description.substring(0, 100).trim() + "...";
 
 		// create favourite selector icon and add to the container
-		let favouriteIcon = card.querySelector("favourite-icon");
+		let favouriteIcon = card.querySelector(".favourite-icon");
 		favouriteIcon.classList.add("fa");
 		favouriteIcon.classList.add("fa-heart");
 
@@ -45,24 +159,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
 		container.appendChild(card);
 	}
-
-	properties.map(prop => {
-		if (prop.favourite) {
-			propertyTileCreator(prop);
-		}
-	});
-})
-
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-
-	let results = [];
-
-	for (let i = 0; i < 3; i++) {
-		results.push(properties[i]);
-	}
-
-	localStorage.setItem("searchResults", JSON.stringify(results));
-
-	window.location.href = "./search-results.html";
-})
+});
--- search-results.js
+++ search-results.js
@@ -3,11 +3,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let searchResultsContainer = document.getElementById("search-results-container");
 
-  let cardTemplate = document.querySelector("[data-card-template]");
+  let cardTemplate = document.getElementById("card-template");
 
   if (searchResults && searchResults.length > 0) {
     searchResults.forEach((result) => {
-      const card = cardTemplate.content.cloneNode(true).children[0];
-    })
+      propertyTileCreator(result);
+    });
+  } else {
+    let error = document.createElement("p");
+    error.textContent = "No properties found.";
+    searchResultsContainer.appendChild(error);
   }
-})
+
+  function propertyTileCreator(prop) {
+    const card = cardTemplate.content.cloneNode(true);
+
+    // create the link
+    const linkElm = card.querySelector("[data-card-link]");
+    linkElm.setAttribute("href", prop.url || "#");
+
+    //create title
+    let propTitle = card.querySelector(".property-description");
+    propTitle.innerText = prop.description.substring(0, 100).trim() + "...";
+
+    // create favourite selector icon and add to the container
+    let favouriteIcon = card.querySelector(".favourite-icon");
+    favouriteIcon.classList.add("fa");
+
+    if (prop.favourite) {
+      favouriteIcon.classList.add("fa-heart");
+    } else {
+      favouriteIcon.classList.add("fa-heart-o");
+    }
+
+    // create image and add to the container
+    let propImage = card.querySelector(".property-image");
+    propImage.src = prop.pictures[0];
+
+    searchResultsContainer.appendChild(card);
+  }
+});
